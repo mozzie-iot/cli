4a18ec54619406bd0b5e3dab94841932eb44b340
tyler smith <tyler.smith.la@gmail.com>
2023-05-22 17:02:31 +0000

    add install check for uninstall command

--- src/commands/uninstall.ts
+++ src/commands/uninstall.ts
@@ -1,11 +1,18 @@
 import { Command } from '@oclif/core';
 import * as inquirer from 'inquirer';
 import { spawn } from 'node:child_process';
+import * as fs from 'node:fs';
 
 export default class Uninstall extends Command {
   static description = 'Uninstall Huebot environment'
 
   async run(): Promise<void> {
+    // Simple check for now - could make this more comprehensive in the future (e.g. check for other artifacts)
+    if (!fs.existsSync('/usr/local/bin/huebot')) {
+      this.log('Huebot directory not found. Are you sure the environment has been installed?');
+      return;
+    }
+
     const { confirm } = await inquirer.prompt({
       type: 'confirm',
       name: 'confirm',
